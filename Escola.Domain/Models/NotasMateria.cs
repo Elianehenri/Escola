551926d2c38ea23cd7fdf847fdf292671451f8d3
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-09-09 16:14:35 -0300

    Adicionar arquivos de projeto.

---
+++ Escola.Domain/Models/NotasMateria.cs
@@ -0,0 +1,27 @@
+﻿using Escola.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Models
+{
+    public class NotasMateria
+    {
+        public int Id { get; set; }
+        public int BoletimId { get; set; }
+        public double Nota { get; set; }
+        public int MateriaId { get; set; }
+        public virtual Materia Materia { get; set; }
+        public virtual Boletim Boletim { get; set; }
+
+        public NotasMateria()
+        {
+        }
+
+       
+
+
+    }
+}
